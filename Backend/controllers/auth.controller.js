f921c03c00bf9a7a55319cbccfbe51ebef692198
Harshroxnox <harshroxnox@gmail.com>
2025-04-04 23:01:37 +0530

    example commit to learn merge conflict

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+export const sendMessageToConsole = (msg) => {
+    console.log(msg);
+}
+
+export const sendMessageToConsole2 = (msg) => {
+    console.log(msg);
+}
+
+export const sendMessageToConsole3 = (msg) => {
+    console.log(msg);
+}
+
+export const sendMessageToConsole4 = (msg) => {
+    console.log(msg);
+}
+
+export const sendMessageToConsole5 = (msg) => {
+    console.log(msg);
+}
+
