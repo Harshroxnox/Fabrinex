bb856b54f5d8eb6efc568a00f3bf9bc4e6fb5957
Harshroxnox <harshroxnox@gmail.com>
2025-04-12 20:40:59 +0530

    admin panels buttons are now dynamic

--- vite-react-app/src/pages/Admin.jsx
+++ vite-react-app/src/pages/Admin.jsx
@@ -3,28 +3,83 @@ import { useState } from 'react'
 import "./Admin.css"
 
 const Admin = () => {
+  const [activeBtn, setActiveBtn] = useState("Home");
+
   return (
     <div className="admin-container">
       <h1>Admin Panel</h1>
 
       <div className="admin-box">
         <div className="admin-sidebar">
-          <button id='home-btn'>Home</button>
-          <button>Orders</button>
-          <button>Products</button>
-          <button>Customers</button>
-          <button>Messaging</button>
-          <button>Promotions</button>
-          <button>Web</button>
-          <button>Analytics</button>
-          <button>Settings</button>
+          <button
+            onClick={()=>setActiveBtn("Home")} 
+            className={`home-btn ${(activeBtn=="Home")?"active-btn":""}`}
+          >
+            Home
+          </button>
+
+          <button 
+            onClick={()=>setActiveBtn("Orders")} 
+            className={(activeBtn=="Orders")?"active-btn":""}
+          >
+            Orders
+          </button>
+
+          <button 
+            onClick={()=>setActiveBtn("Products")} 
+            className={(activeBtn=="Products")?"active-btn":""}
+          >
+            Products
+          </button>
+
+          <button 
+            onClick={()=>setActiveBtn("Customers")} 
+            className={(activeBtn=="Customers")?"active-btn":""}
+          >
+            Customers
+          </button>
+          
+          <button 
+            onClick={()=>setActiveBtn("Messaging")} 
+            className={(activeBtn=="Messaging")?"active-btn":""}
+          >
+            Messaging
+          </button>
+
+          <button 
+            onClick={()=>setActiveBtn("Promotions")} 
+            className={(activeBtn=="Promotions")?"active-btn":""}
+          >
+            Promotions
+          </button>
+          
+          <button 
+            onClick={()=>setActiveBtn("Web")} 
+            className={(activeBtn=="Web")?"active-btn":""}
+          >
+            Web
+          </button>
+
+          <button 
+            onClick={()=>setActiveBtn("Analytics")} 
+            className={(activeBtn=="Analytics")?"active-btn":""}
+          >
+            Analytics
+          </button>
+       
+          <button 
+            onClick={()=>setActiveBtn("Settings")} 
+            className={(activeBtn=="Settings")?"active-btn":""}
+          >
+            Settings
+          </button>
         </div>
         <div className="admin-main">
-          <p>This will be the barcode section for inventory management</p> 
+          <p>{`Currently you have selected ${activeBtn}`}</p> 
           <p>This section will keep on changing depending upon what is selected from side panel</p> 
         </div>
       </div>
-      
+
     </div>
   )
 }
