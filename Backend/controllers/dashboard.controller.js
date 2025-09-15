d7066e86095cf265a586bdd218657b6674689c86
RAHUl-CHAUDHARY01 <chaudharyrahul9315@gmail.com>
2025-09-15 13:53:51 +0530

    dashboard get best selling price correction

--- Backend/controllers/dashboard.controller.js
+++ Backend/controllers/dashboard.controller.js
@@ -15,17 +15,24 @@ export const getMetrics = async (req, res, next) => {
 export const getBestSellingPricePerVariant = async (req, res, next) => {
   try {
     const [rows] = await db.execute(`
-      SELECT t.variantID , t.price_at_purchase , t.total_sold
-      FROM(
-        SELECT 
-          variantID , 
-          price_At_purchase,
-          SUM(quantity) AS total_sold,
-          ROW_NUMBER() OVER (PARTITION BY variantID ORDER BY SUM(quantity) DESC) AS rn
-        FROM OrderItems
-        GROUP BY variantID , price_at_purchase
+      SELECT t.variantID, 
+       p.name, 
+       v.size, 
+       v.color, 
+       t.price_at_purchase, 
+       t.total_sold
+      FROM (
+          SELECT 
+              variantID,
+              price_at_purchase,
+              SUM(quantity) AS total_sold,
+              ROW_NUMBER() OVER (PARTITION BY variantID ORDER BY SUM(quantity) DESC) AS rn
+          FROM OrderItems
+          GROUP BY variantID, price_at_purchase
       ) t
-       WHERE t.rn = 1;
+      JOIN ProductVariants v ON v.variantID = t.variantID
+      JOIN Products p ON p.productID = v.productID
+      WHERE t.rn = 1;
     `);
     res.status(200).json({
       message: "Best selling price per variant fetched successfully",
